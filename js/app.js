791421b7d7d54e6c91dc7171ed7dbfcd2838d2d2
Salome M <101974223+salomemosiava@users.noreply.github.com>
2022-10-01 18:14:38 +0400

    bugfix

--- js/app.js
+++ js/app.js
@@ -1,24 +1,8 @@
-(() => {
-    'use strict'
-    const forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.from(forms).forEach(form => {
-      form.addEventListener('change', event => {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-        form.classList.add('was-validated')
-      }, false)
-    })
-  })()
 const button = document.getElementById('button');
 const localStorageKey = 'inputData';
 
 let table = document.getElementById('table');
-let row = 1;
-let index = 1;
+let row;
 
 let firstName = document.getElementById('validationCustom01');
 let lastName = document.getElementById('validationCustom02');
@@ -27,89 +11,43 @@ let birthday = document.getElementById('birthday');
 let gender = document.getElementById('validationCustom04');
 let notes = document.getElementById('floatingTextarea2');
 let modal = document.querySelector('.modal');
+let savedForms = JSON.parse(localStorage.getItem("forms"));
+let index = savedForms === null ? 0 : Number(localStorage.index);
+let submitedForms = savedForms === null ? [] : savedForms;
 
-const sendStorage = (item) => {
-  item.addEventListener('change', () => {
-    localStorage.setItem(`${item.id}`, item.value);
-  });
- };
- sendStorage(firstName);
- sendStorage(lastName);
- sendStorage(address);
- sendStorage(birthday);
- sendStorage(gender);
- sendStorage(notes);
+console.log(submitedForms);
 
- firstName.value = localStorage.getItem('validationCustom01');
- lastName.value = localStorage.getItem('validationCustom02');
- address.value = localStorage.getItem('validationCustom03');
- birthday.value = localStorage.getItem('birthday');
- gender.value = localStorage.getItem('validationCustom04');
- notes.textContent = localStorage.getItem('floatingTextarea2');
-
-
-
-
-
+const inputArr = [firstName, lastName, address, birthday, gender, notes]
 button.addEventListener('click', (e) => {
-  
     e.preventDefault();
 
-    const deletebtn = document.createElement('div');
-    deletebtn.textContent = 'X';
-    deletebtn.style.backgroundColor = 'red';
-    deletebtn.style.color = 'white';
-    deletebtn.style.padding = '0.25rem';
-    deletebtn.style.textAlign = 'center';
-
-
-    let newRow = table.insertRow(row);
-
-
-    if (!firstName.value || !lastName.value || !address.value) {
-        event.preventDefault();
+    const filledArr = inputArr.filter((input) => input.value.trim() !== "");
+    
+    if (!/^[A-z]+$/.test(firstName.value)) {
+      firstName.style.border = '2px solid red'
+      return;
     }
-    else {
-      
-        let cellOne = newRow.insertCell(0);
-        let cellTwo = newRow.insertCell(1);
-        let cellThree = newRow.insertCell(2);
-        let cellFour = newRow.insertCell(3);
-        let cellFive = newRow.insertCell(4);
-        let cellSix = newRow.insertCell(5);
-        let cellSeven = newRow.insertCell(6);
-        let modalText = document.getElementById('modal-text');
-
-        cellOne.innerHTML = index;
-        cellTwo.innerHTML = localStorage.getItem('validationCustom01');
-        cellThree.innerHTML = localStorage.getItem('validationCustom02');
-        cellFour.innerHTML = localStorage.getItem('validationCustom03');
-        cellFive.innerHTML = localStorage.getItem('birthday');
-        cellSix.innerHTML = localStorage.getItem('validationCustom04');
-        cellSeven.append(deletebtn);
-        modalText .textContent = localStorage.getItem('floatingTextarea2');
-        index++;
-
-        newRow.addEventListener('click', () => {
-          if (event.target.innerHTML !== 'X') {
-            modal.style.display = 'block';
-            modal.style.top = '80vh';
-          }
-        });
-
-        deletebtn.addEventListener('click', (e) => {
-          e.preventDefault();
-          cellOne.remove();
-          cellTwo.remove();
-          cellThree.remove();
-          cellFour.remove();
-          cellFive.remove();
-          cellSix.remove();
-          cellSeven.remove();
-          index--;
-      });
+  
+    if (!/^[A-z]+$/.test(lastName.value)) {
+      lastName.style.border = '2px solid red'
+      return;
     }
+    if (filledArr){
+      index ++;
+      submitedForms.push({
+        index,
+        form: {
+          firstName: firstName.value,
+          lastName: lastName.value,
+          adress: address.value,
+          date: birthday.value,
+          gender: gender.value,
+          note: notes.value,
+        },
+      })};
 
+    localStorage.setItem('forms', JSON.stringify(submitedForms));
+    localStorage.setItem('index', JSON.stringify(index));
 
     row++;
     firstName.value = '';
@@ -118,9 +56,114 @@ button.addEventListener('click', (e) => {
     birthday.value = '';
     gender.value = '';
     notes.value = '';
+    location.reload();
 });
+
+
+const deletebtn = document.createElement('div');
+deletebtn.textContent = 'X';
+deletebtn.style.backgroundColor = 'red';
+deletebtn.style.color = 'white';
+deletebtn.style.padding = '0.25rem';
+deletebtn.style.textAlign = 'center';
 
 let close = document.querySelector('.close');
 close.addEventListener('click', ()  => {
   modal.style.display = 'none';
 });
+
+
+
+savedForms.forEach((item) => {
+  let newRow = table.insertRow(row);
+      
+  let cellOne = newRow.insertCell(0);
+  let cellTwo = newRow.insertCell(1);
+  let cellThree = newRow.insertCell(2);
+  let cellFour = newRow.insertCell(3);
+  let cellFive = newRow.insertCell(4);
+  let cellSix = newRow.insertCell(5);
+  let cellSeven = newRow.insertCell(6);
+  let modalText = document.getElementById('modal-text');
+
+  cellOne.innerHTML = item.index;
+  cellTwo.innerHTML = item.form.firstName;
+  cellThree.innerHTML =  item.form.lastName;
+  cellFour.innerHTML =  item.form.adress;
+  cellFive.innerHTML =  item.form.firstName.date;
+  cellSix.innerHTML =  item.form.gender;
+  modalText .textContent = item.form.note
+      
+  const deletebtn = document.createElement('div');
+  deletebtn.textContent = 'X';
+  deletebtn.style.backgroundColor = 'red';
+  deletebtn.style.color = 'white';
+  deletebtn.style.padding = '0.25rem';
+  deletebtn.style.textAlign = 'center';
+  deletebtn.style.cursor = 'pointer';
+
+  cellSeven.append(deletebtn);
+
+
+  deletebtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log(submitedForms.splice(item.id-1,1));
+  localStorage.setItem('forms', JSON.stringify(submitedForms));  
+  cellOne.remove();
+  cellTwo.remove();
+  cellThree.remove();
+  cellFour.remove();
+  cellFive.remove();
+  cellSix.remove();
+  cellSeven.remove();
+  index--;
+  localStorage.setItem('index', JSON.stringify(index));
+});
+  newRow.addEventListener('click', () => {
+    if (event.target.innerHTML !== 'X') {
+      modal.style.display = 'block';
+      modal.style.top = '80vh';
+    }
+  });
+})
+// const createSubmitedForm = () =>  {
+//   let newRow = table.insertRow(row);
+
+      
+//   let cellOne = newRow.insertCell(0);
+//   let cellTwo = newRow.insertCell(1);
+//   let cellThree = newRow.insertCell(2);
+//   let cellFour = newRow.insertCell(3);
+//   let cellFive = newRow.insertCell(4);
+//   let cellSix = newRow.insertCell(5);
+//   let cellSeven = newRow.insertCell(6);
+//   let modalText = document.getElementById('modal-text');
+
+//   cellOne.innerHTML = index;
+//   cellTwo.innerHTML = localStorage.getItem('validationCustom01');
+//   cellThree.innerHTML = localStorage.getItem('validationCustom02');
+//   cellFour.innerHTML = localStorage.getItem('validationCustom03');
+//   cellFive.innerHTML = localStorage.getItem('birthday');
+//   cellSix.innerHTML = localStorage.getItem('validationCustom04');
+//   cellSeven.append(deletebtn);
+//   modalText .textContent = localStorage.getItem('floatingTextarea2');
+
+//   newRow.addEventListener('click', () => {
+//     if (event.target.innerHTML !== 'X') {
+//       modal.style.display = 'block';
+//       modal.style.top = '80vh';
+//     }
+//   });
+
+//   deletebtn.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     cellOne.remove();
+//     cellTwo.remove();
+//     cellThree.remove();
+//     cellFour.remove();
+//     cellFive.remove();
+//     cellSix.remove();
+//     cellSeven.remove();
+//     index--;
+// });
+// }
